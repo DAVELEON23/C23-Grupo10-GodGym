5cba44505610d8a9d288e2068133f7a0df78e373
diego isray <diegoisray2@gmail.com>
2023-12-05 18:16:30 -0300

    vista de productos creada falta editarla

---
+++ C23-Grupo10-GodGym2.0/src/controllers/productsController.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+const json = fs.readFileSync(path.join(__dirname,"../database/products.json"),"utf-8")
+const products = JSON.parse(json);
+
+
+const productsController = {
+      detail: (req, res) => {
+        const {id} = req.params;
+        const product = products.find(producto => producto.id == id);
+        res.render('products/detail', { title: product.nombre, products });
+    },
+}
+    
+module.exports = productsController
