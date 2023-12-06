250f4105b81378851c7b0796a43c9de892f76d92
diego isray <diegoisray2@gmail.com>
2023-12-05 21:37:18 -0300

    cree las vistas de users/register y users/login, tambien edite algo en el css y el html

--- C23-Grupo10-GodGym2.0/src/routes/users.js
+++ C23-Grupo10-GodGym2.0/src/routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {registro,login} = require('../controllers/usersController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router
+.get('/register', registro)
+.get('/login', login)
 
 module.exports = router;
