598f3d1269f7adf017feae253cfb78c7e55c3fbb
diego isray <diegoisray2@gmail.com>
2023-12-05 21:49:12 -0300

    ruta de carrito creada

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.get('/', (req,res) => {
 app.get('/productDetail', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/productDetail.html'))
 })
+app.get('/productCart', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/productCart.html'))
+})
 
 app.get('/register', (req,res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'))
