d47c49ff27f1befbf303073d0a4da196110aafc1
diego isray <diegoisray2@gmail.com>
2023-12-18 19:20:05 -0300

    cree las rutas de los metodos del crud

--- C23-Grupo10-GodGym2.0/src/controllers/productsController.js
+++ C23-Grupo10-GodGym2.0/src/controllers/productsController.js
@@ -11,40 +11,61 @@ const getJson = () =>{
 
 
 const productsController = {
-  detail: (req, res) => {
+
+    detail: (req, res) => {
     const {id} = req.params;
     const products = getJson()
     const product = products.find(product => product.id == id);
     res.render('products/detail', { title: product.nombre, product});
-  },
+    },
 
     actividades: (req, res) => {
     const {id} = req.params;
-  const products = getJson()
-  res.render('products', { title: 'GOD GYM', products });
-  },
+    const products = getJson()
+    res.render('products', { title: 'GOD GYM', products });
+    },
+
     productCart: (req, res) => {
       res.render('products/cart', { title: 'GOD GYM', products });
     },
+
     dashboard:(req, res) => {
       res.render('products/dashboard', { title: 'dashboard', products });
     },
-    edit:(req, res) => {
+
+    // vista formulario de edicion
+    productEditView:(req, res) => {
       const {id} = req.params;
       const products = products.find(elemento => elemento.id == id );
       res.render('products/productEdit', { title: 'Editar', products });
     },
-    create: (req,res)=>{
-      const product = req.body 
-      product.id = products[products.length-1].id +1;
-      products.push(product);
-      const productJson = JSON.stringify(products);
-      fs.writeFileSync(path.join(__dirname,"../database/product.json"),productJson,"utf-8");
-      res.redirect("/products/dashboard")
-  },
-  productCreateView: (req,res)=>{
+
+    //metodo de edicion
+    edit: (req,res) =>{
+
+    },
+
+    // vista formulario de creacion
+    productCreateView: (req,res)=>{
     res.render("products/productCreate",{ title: "Crear"});
-},
-}
+    } ,
+
+    //metodo de creacion
+    create: (req,res)=>{
+    const product = req.body 
+    product.id = products[products.length-1].id +1;
+    products.push(product);
+    const productJson = JSON.stringify(products);
+    fs.writeFileSync(path.join(__dirname,"../database/product.json"),productJson,"utf-8");
+    res.redirect("/products/dashboard")
+    } ,
     
+    //metodo de eliminacion
+    productDelete: (req,res)=>{
+
+    }
+  }
+    
+
+  
 module.exports = productsController;
