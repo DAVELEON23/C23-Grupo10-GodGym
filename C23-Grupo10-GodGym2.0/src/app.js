e6c308847a3cfb9d35388550380a36f8877aede3
DAVELEON23 <davidaarondeleon@gmail.com>
2023-12-18 21:52:15 -0300

    Se realizo la logica del metodo DELETE

--- C23-Grupo10-GodGym2.0/src/app.js
+++ C23-Grupo10-GodGym2.0/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const methodOverride = require("method-override");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-
+app.use(methodOverride('_method'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter)
@@ -39,5 +40,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
 
 module.exports = app;
