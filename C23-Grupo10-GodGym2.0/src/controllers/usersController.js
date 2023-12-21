3408c1167eeb2b42793e304162f0e55c98048b23
diegoisray <diegoisray2@gmail.com>
2023-12-21 14:14:04 -0300

    cree el json de usuarios y logica para que se guarden, ademas edite el formulario de registro

--- C23-Grupo10-GodGym2.0/src/controllers/usersController.js
+++ C23-Grupo10-GodGym2.0/src/controllers/usersController.js
@@ -1,7 +1,37 @@
+const { json } = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const usersFilePath = path.join(__dirname, '../database/Users.json');
+
+const UsersJson = () =>{
+	const usersFilePath = path.join(__dirname, '../database/Users.json');
+	const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+	return users
+}
+
 const usersController = {
-    registro: (req, res) => {
-        res.render('users/register', { title: 'GOD GYM'});
+    viewRegister: (req, res) => {
+      const users = UsersJson()
+        res.render('users/register', { title: 'GOD GYM', users});
       },
+      createRegister:(req,res) =>{
+        const users = UsersJson()
+        const {nombre,apellido,fecha,email,contraseña} = req.body
+      
+        const newUser = {
+          nombre: nombre.trim(),
+          apellido: apellido.trim(),
+          fecha,
+          email:email.trim(),
+          contraseña: contraseña.trim()
+        }
+        users.push(newUser)
+        const jsonUsers = JSON.stringify(users)
+        fs.writeFileSync(usersFilePath, jsonUsers, 'utf-8')
+        res.redirect('/')
+      },
+
       login: (req, res) => {
         res.render('users/login', { title: 'GOD GYM'});
       },
