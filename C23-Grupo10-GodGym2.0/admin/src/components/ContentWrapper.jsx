b2f512f055a91f07ec9e60c0a2769812399f08d6
DAVELEON23 <davidaarondeleon@gmail.com>
2024-04-13 23:53:30 -0300

    Se comenzo con react adaptaciones de codigo.Ruta para dashboard de usuarios y de productos. Boton para ir a home. Logo, falta css

---
+++ C23-Grupo10-GodGym2.0/admin/src/components/ContentWrapper.jsx
@@ -0,0 +1,16 @@
+import ContentRowTop from './ContentRowTop'
+import Footer from './Footer'
+
+
+
+export default function ContentWrapper() {
+    return(
+                <div id="content-wrapper" className="d-flex flex-column">
+                        <div id="content">
+                            
+                            <ContentRowTop/>
+                        </div>
+                            <Footer/>
+                </div>
+    )
+}
