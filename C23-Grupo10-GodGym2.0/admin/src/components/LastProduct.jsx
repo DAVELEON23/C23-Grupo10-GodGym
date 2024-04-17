9fcbcfde9b8888ee81b30aeaf6a106613375254b
Gustavo Insfran <gujain80@gmail.com>
2024-04-17 09:21:28 -0300

    cambios  lastproduct

--- C23-Grupo10-GodGym2.0/admin/src/components/LastProduct.jsx
+++ C23-Grupo10-GodGym2.0/admin/src/components/LastProduct.jsx
@@ -11,7 +11,7 @@ export default function LastMovie({nombre,info,imagen,id}) {
 		
 							<div className="card shadow mb-4">
 								<div className="card-header py-3">
-									<h5 className="m-0 font-weight-bold text-gray-800">{nombre}</h5>
+									<h5 className="m-0 font-weight-bold text-gray-800">Ultimo agregado: {nombre}</h5>
 								</div>
 			<div className="card-body">
 				<div className="text-center">
